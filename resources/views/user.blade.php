189259c7ac83156d409349c977dc1d2cb0f64e85
Guilherme Souza <guilherme.souza@digitalseller.com.br>
2022-03-03 23:34:58 -0300

    Alterações no env e gitignore para subir o env

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Welcome to CodeIgniter 4 + React.js!</title>
+	<title>CRUD - SEMGE (Av. Prática)</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<style> .navbar{margin-bottom: 20px;} </style>
 </head>
 <body>
 <div class="container" style="padding:20px;">
 	<h1 style="text-align:center;">
-		<a href="/user"> Full Stack - Laravel 8 & React Hooks </a>
+		<a href="/user"> Sistema CRUD - SEMGE </a>
 	</h1>
 	<hr>
 
